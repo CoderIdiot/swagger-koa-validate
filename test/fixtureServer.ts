01e0d4dc5cb01011db290cec6167f20d104e0e12
fengzhigang <fengzg@polyvi.com>
2017-11-02 15:48:30 +0800

    resolve $ref

--- test/fixtureServer.ts
+++ test/fixtureServer.ts
@@ -7,25 +7,34 @@ const trace = Trace('trace')
 import * as fs from "fs-extra"
 const YAML = require('yamljs')
 
-import ValidatorFactory from '../src/spec'
+const $RefParser = require('json-schema-ref-parser')
+var parser = new $RefParser()
 
-const specString = fs.readFileSync(__dirname + '/../../test/api.yaml', 'utf-8')
-const spec = YAML.parse(specString)
-const validate = ValidatorFactory(spec)
+import ValidatorFactory from '../src/spec'
 
 import * as Koa from "koa"
 const app = new Koa()
 
-app.use(async (ctx, next) => {
-    try {
-        await next()
-    } catch (error) {
-        ctx.status = error.status || 400
-        ctx.body = { message: error.message }
-        trace(error)
-    }
-})
-app.use(validate)
+const specString = fs.readFileSync(__dirname + '/../../test/api.yaml', 'utf-8')
+const spec = YAML.parse(specString)
+var validate
+
+parser.dereference(__dirname + '/../../test/api.yaml')
+    .then(function (spec) {
+        validate = ValidatorFactory(spec)
+        app.use(async (ctx, next) => {
+            try {
+                await next()
+            } catch (error) {
+                ctx.status = error.status || 400
+                ctx.body = { message: error.message }
+                trace(error)
+            }
+        })
+        app.use(validate)
+    })
+    .catch(console.log)
+
 const request = require("supertest")(app.listen(3000))
 
 export default request
