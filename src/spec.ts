09f24d3052242109a3f56374b4ef503e9c208a79
HaiChiang <rq8686@qq.com>
2017-11-12 18:57:48 +0800

    Done Check Required. Need refactor.

--- src/spec.ts
+++ src/spec.ts
@@ -1,100 +1,143 @@
-import * as Trace from 'debug-trace-fn'
-const trace = Trace('trace')
+import * as Trace from "debug-trace-fn"
+const trace = Trace("trace")
 
-import * as R from 'ramda'
-import * as pathToRegexp from 'path-to-regexp'
+import * as R from "ramda"
+import * as pathToRegexp from "path-to-regexp"
 
-import * as Ajv from 'ajv'
-const ajv = new Ajv({ coerceTypes: 'array' })
+import * as Ajv from "ajv"
+const ajv = new Ajv({ coerceTypes: "array" })
 
-ajv.addFormat('int32', (value) => Number.isInteger(+value))
-ajv.addFormat('int64', (value) => Number.isInteger(+value))
+ajv.addFormat("int32", value => Number.isInteger(+value))
+ajv.addFormat("int64", value => Number.isInteger(+value))
 
-export default (spec: object) =>
-    async (ctx, next) => {
-        ctx.swagger = { paths: preCompile(spec['paths']) }
-        checkRequest(ctx)
-        await next()
-    }
+export default (spec: object) => async (ctx, next) => {
+  ctx.swagger = { paths: preCompile(spec["paths"]) }
+  checkRequest(ctx)
+  await next()
+}
 
 function checkRequest(ctx) {
-    // trace(ctx)
-    // trace(ctx.swagger)
+  checkPath(ctx)
+  checkMethod(ctx)
+  checkRequired(ctx)
 
-    checkPath(ctx)
-    checkMethod(ctx)
-    checkParamsInPath(ctx)
+  checkParamsInPath(ctx)
+  checkQueryString(ctx)
+  checkHeader(ctx)
 
-    checkQueryString(ctx)
+  // trace(ctx.swagger)
 
-    checkHeader(ctx)
-
-    // trace(ctx.swagger)
-    // trace(ctx)
-
-    ctx.status = 200
+  ctx.status = 200
 }
 
-const preCompile =
-    R.mapObjIndexed((value, key, obj) => {
-        let keys = []
-        let reg = pathToRegexp(R.replace('}', '', R.replace('{', ':', key)), keys)
+const preCompile = R.mapObjIndexed((value, key, obj) => {
+  let keys = []
+  let reg = pathToRegexp(R.replace("}", "", R.replace("{", ":", key)), keys)
 
-        return R.merge(value, { 'urlReg': reg, 'urlKeys': R.map((value) => value['name'], keys) })
-    })
+  return R.merge(value, {
+    urlReg: reg,
+    urlKeys: R.map(value => value["name"], keys)
+  })
+})
 
-const checkPath = (ctx) => {
-    let pathBody = R.find(
-        ([path, pathBody]) => {
-            let params = pathBody['urlReg'].exec(ctx.URL.pathname)
-            if (!params) return false
-            ctx.swagger.paramsInUrl = R.zipObj(pathBody['urlKeys'], R.tail(params))
-            ctx.swagger.pathBody = pathBody
-            return true
-        },
-        R.toPairs(ctx.swagger.paths)
-    )
+const checkPath = ctx => {
+  let pathBody = R.find(([path, pathBody]) => {
+    let params = pathBody["urlReg"].exec(ctx.URL.pathname)
+    if (!params) return false
+    ctx.swagger.paramsInUrl = R.zipObj(pathBody["urlKeys"], R.tail(params))
+    ctx.swagger.pathBody = pathBody
+    return true
+  }, R.toPairs(ctx.swagger.paths))
 
-    if (!pathBody)
-        throw { status: 404 }
+  if (!pathBody) throw { status: 404 }
 }
 
-const checkMethod = (ctx) => {
-    let { method, swagger } = ctx
-    method = R.toLower(method)
+const checkMethod = ctx => {
+  let { method, swagger } = ctx
+  method = R.toLower(method)
 
-    if (!R.has(method, swagger.pathBody))
-        throw { status: 501 }
-    swagger.methodBody = swagger.pathBody[method]
-    swagger.methodBody.parameters = swagger.methodBody.parameters || {}
+  if (!R.has(method, swagger.pathBody)) throw { status: 501 }
+  swagger.methodBody = swagger.pathBody[method]
+  swagger.methodBody.parameters = swagger.methodBody.parameters || {}
 }
 
-const checkParamsInPath = (ctx) => checkParam(ctx, 'path', ctx.swagger.paramsInUrl)
+const checkRequired = ctx => {
+  let path = R.find(([path, pathBody]) => {
+    let params = pathBody["urlReg"].exec(ctx.URL.pathname)
+    if (!params) return false
+    return true
+  }, R.toPairs(ctx.swagger.paths))
 
-const checkQueryString = (ctx) => checkParam(ctx, 'query', trace(ctx.query))
+  let params = []
+  R.forEach(value => {
+    R.forEachObjIndexed((v, k) => {
+      if (R.has("parameters", v))
+        R.forEach(p => params.push(p), v["parameters"])
+    }, value)
+  }, path)
 
-const checkHeader = (ctx) => checkParam(ctx, 'header', ctx.header)
+  R.forEach(value => {
+      if (R.has("required", value))
+          checkParamExist(ctx, value['in'], value['name'])    
+  }, params)
+}
 
-const findParam = (ctx, paramIn, paramName) => R.find(
-    (param) => (param['in'] == paramIn && param['name'] == paramName),
+const checkParamExist = (ctx, paramIn, paramName) => {
+  trace(ctx.header, 'header')
+  switch (paramIn) {
+    case 'header': {
+      if (R.has(paramName, ctx.header)) break;
+      throw {
+        status: 400,
+        message:
+            "Invalid Required Param " + paramName + " for parameter in " + paramIn
+      }
+    }
+    case 'path': {
+      break;
+    }  
+    default: {
+      throw {
+      status: 400,
+      message:
+          "Invalid Required Param " + paramName + "for parameter in " + paramIn
+    }}  
+  }
+}
+
+const checkParamsInPath = ctx =>
+  checkParam(ctx, "path", ctx.swagger.paramsInUrl)
+
+const checkQueryString = ctx => checkParam(ctx, "query", ctx.query)
+
+const checkHeader = ctx => checkParam(ctx, "header", ctx.header)
+
+const findParam = (ctx, paramIn, paramName) =>
+  R.find(
+    param => param["in"] == paramIn && param["name"] == paramName,
     ctx.swagger.methodBody.parameters
-)
+  )
 
 const checkParam = (ctx, paramIn, parameters) => {
-    return R.find(
-        ([paramName, value]) => {
-            let paramSpec = findParam(ctx, paramIn, paramName)
+  return R.find(([paramName, value]) => {
+    let paramSpec = findParam(ctx, paramIn, paramName)
 
-            if (!paramSpec) return false
+    if (!paramSpec) return false
 
-            if (!ajv.validate(paramSpec['schema'], value)) {
-                throw {
-                    status: 400,
-                    message: 'Invalid format for parameter in ' + paramIn + ' {' + paramName + '}, received: ' + value + '.  errors:' + ajv.errorsText()
-                }
-            }
-            return false
-        },
-        R.toPairs(parameters)
-    )
+    if (!ajv.validate(paramSpec["schema"], value)) {
+      throw {
+        status: 400,
+        message:
+          "Invalid format for parameter in " +
+          paramIn +
+          " {" +
+          paramName +
+          "}, received: " +
+          value +
+          ".  errors:" +
+          ajv.errorsText()
+      }
+    }
+    return false
+  }, R.toPairs(parameters))
 }
--- test/path.test.ts
+++ test/path.test.ts
@@ -1,45 +1,50 @@
-import * as assert from "assert"
-import { suite, test } from "mocha-typescript"
+import * as assert from "assert";
+import { suite, test } from "mocha-typescript";
 
-import request from './fixtureServer'
+import request from "./fixtureServer";
 
 @suite("path check")
 class PathTest {
-    @test("correct url")
-    async testPaths() {
-        let res = await request.get('/user')
-        assert.equal(res.statusCode, 200)
-    }
+  @test("correct url")
+  async testPaths() {
+    let res = await request.get("/user").
+    set({
+        "id": 100
+    });
+    assert.equal(res.statusCode, 200);
+  }
 
-    @test("404")
-    async test404() {
-        let res = await request.get('/404')
-        assert.equal(res.statusCode, 404)
-    }
+  @test("404")
+  async test404() {
+    let res = await request.get("/404");
+    assert.equal(res.statusCode, 404);
+  }
 
-    @test("getUserById")
-    async testGetUserById() {
-        let res = await request.get('/user/100')
-        assert.equal(res.statusCode, 200)
-    }
+  @test("getUserById")
+  async testGetUserById() {
+    let res = await request.get("/user/100");
+    assert.equal(res.statusCode, 200);
+  }
 
-    @test("getUserByWrongId")
-    async testGetUserByWrongId() {
-        let res = await request.get('/user/abc')
-        assert.equal(res.statusCode, 400)
-        assert.equal(res.body.message, 'Invalid format for parameter in path {id}, received: abc.  errors:data should be integer')
-    }
+  @test("getUserByWrongId")
+  async testGetUserByWrongId() {
+    let res = await request.get("/user/abc");
+    assert.equal(res.statusCode, 400);
+    assert.equal(
+      res.body.message,
+      "Invalid format for parameter in path {id}, received: abc.  errors:data should be integer"
+    );
+  }
 
-    @test("wrong method")
-    async testWrongMethod() {
-        let res = await request.post('/user')
-        assert.equal(res.statusCode, 501)
-    }
+  @test("wrong method")
+  async testWrongMethod() {
+    let res = await request.post("/user");
+    assert.equal(res.statusCode, 501);
+  }
 
-    @test("path with queryString")
-    async testPathWithQueryString() {
-        let res = await request.post('/user/action/login')
-        assert.equal(res.statusCode, 200)
-    }
-
+  @test("path with queryString")
+  async testPathWithQueryString() {
+    let res = await request.post("/user/action/login");
+    assert.equal(res.statusCode, 200);
+  }
 }
